1f42738a21108efe1c8c2ce1c6af372166c5646b
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-09-22 17:59:49 +0530

    '--changes--'

--- PollApp01/urls.py
+++ PollApp01/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('PollApp/',include('PollApp.urls')),
+    path('Polls/',include('PollApp.urls')),
     path('admin/', admin.site.urls),
 ]
