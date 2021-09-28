a3feba7f43b08e3530e2b1010e93324d1c44bfd5
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-09-28 23:34:38 +0530

    '--changes--'

--- PollApp/views.py
+++ PollApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404,render
-from django.http import HttpResponse ,Http404
+from django.http import HttpResponse ,Http404,HttpResponseRedirect
 # from django.template import loader
-from .models import Question
+from .models import Question,Choice
+from django.urls import reverse
 
 def index(request):
     latestQuestionList = Question.objects.order_by('pub_date')[:5]
@@ -18,4 +19,18 @@ def results(request, question_id):
     return HttpResponse("You're looking at the results of question %s." % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'PollApp/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('PollApp:results', args=(question.id,)))
