ca613a2f4cbfa05ecc1f68ba1103afc9d735a2de
anuragshukla07 <anuragshukla2k12@gmail.com>
2021-09-30 00:17:01 +0530

    '--some_changes--'

--- PollApp/views.py
+++ PollApp/views.py
@@ -16,7 +16,8 @@ def detail(request, question_id):
     return render(request,'PollApp/detail.html', {'question':question})
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'PollApp/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
